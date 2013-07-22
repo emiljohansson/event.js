a25c041906db446e9cfba7dea0f883ffd58983b0
Emil Johansson <emil@meep.msi.vxu.se>
2013-07-22 08:45:06 +0200

    moduled

--- event.js
+++ event.js
@@ -1,73 +1,76 @@
 /**
- *	https://github.com/emiljohansson/
- *	
- *	@version	0.1.2
- *	@copyright	Emil Johansson, http://emiljohansson.se/
- *	@author		Emil Johansson <emiljohansson.se@gmail.com>
- *	@date		Aug 12, 2012
+ *  https://github.com/emiljohansson/event.js
+ *  
+ *  @version  0.1.2
+ *  @author   Emil Johansson <emiljohansson.se@gmail.com>
+ *  @date     Aug 12, 2012
  */
+(function() {
 
-window.Event = window.Event || {};
+	var Event = window.Event || {};
 
-Event.addEventListenerToArray = function(arr, type, listener) {
-	for (var i = 0; i < arr.length; i++) {
-		var elem = arr[i];
-		Event.addEventListener(elem, type, listener);
+	Event.addEventListenerToArray = function(elemList, type, listener) {
+		for (var i = 0; i < elemList.length; i++) {
+			var elem = elemList[i];
+			Event.addEventListener(elem, type, listener);
+		}
 	};
-}
 
-Event.addEventListener = function(elem, type, listener) {
-	if (Object.prototype.toString.call( elem ) === '[object Array]') {
-		Event.addEventListenerToArray(elem, type, listener);
-		return;
-	}
-	if (!elem) {
-		return;
-	}
-	if (elem.addEventListener) {
-		elem.addEventListener(type, listener, false);
-	}
-	else if (elem.attachEvent) {
-		elem.attachEvent('on'+type, listener);
-	}
-	return elem;
-};
+	Event.addEventListener = function(elem, type, listener) {
+		if (Object.prototype.toString.call( elem ) === '[object Array]') {
+			Event.addEventListenerToArray(elem, type, listener);
+			return;
+		}
+		if (!elem) {
+			return;
+		}
+		if (elem.addEventListener) {
+			elem.addEventListener(type, listener, false);
+		}
+		else if (elem.attachEvent) {
+			elem.attachEvent('on'+type, listener);
+		}
+		return elem;
+	};
 
-Event.removeEventListener = function(elem, type, listener) {
-	if (!elem || !listener) {
-		return;
-	}
-	if (elem.removeEventListener) {
-		elem.removeEventListener(type, listener);
-	}
-	else if (elem.detachEvent) {
-		elem.detachEvent('on'+type, listener);
-	}
-	return elem;
-};
+	Event.removeEventListener = function(elem, type, listener) {
+		if (!elem || !listener) {
+			return;
+		}
+		if (elem.removeEventListener) {
+			elem.removeEventListener(type, listener, false);
+		}
+		else if (elem.detachEvent) {
+			elem.detachEvent('on'+type, listener);
+		}
+		return elem;
+	};
 
-Event.triggerEvent = function(elem, type) {
-	if (!elem) {
-		return;
-	}
-	var event;
-	if (document.createEvent) {
-		event = document.createEvent("HTMLEvents");
-		event.initEvent(type, true, true);
-	}
-	else {
-		event = document.createEventObject();
-		event.eventType = type;
-	}
-	if (document.createEvent) {
-		elem.dispatchEvent(event);
-	}
-	else {
-		elem.fireEvent("on" + event.eventType, event);
-	}
-	return elem;
-};
+	Event.triggerEvent = function(elem, type) {
+		if (!elem) {
+			return;
+		}
+		var event;
+		if (document.createEvent) {
+			event = document.createEvent("HTMLEvents");
+			event.initEvent(type, true, true);
+		}
+		else {
+			event = document.createEventObject();
+			event.eventType = type;
+		}
+		if (document.createEvent) {
+			elem.dispatchEvent(event);
+		}
+		else {
+			elem.fireEvent("on" + event.eventType, event);
+		}
+		return elem;
+	};
 
-Event.currentTarget = function(event) {
-	return !!event.currentTarget ? event.currentTarget : event.srcElement;
-};
+	Event.currentTarget = function(event) {
+		return !!event.currentTarget ? event.currentTarget : event.srcElement;
+	};
+
+	window.Event = Event;
+}());

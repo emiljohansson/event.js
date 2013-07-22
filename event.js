ce07fff3f341811a2519af3fd7ea5f5c07388ac9
Emil Johansson <emil@meep.msi.vxu.se>
2013-07-22 08:17:37 +0200

    first submit

---
+++ event.js
@@ -0,0 +1,73 @@
+/**
+ *	https://github.com/emiljohansson/
+ *	
+ *	@version	0.1.2
+ *	@copyright	Emil Johansson, http://emiljohansson.se/
+ *	@author		Emil Johansson <emiljohansson.se@gmail.com>
+ *	@date		Aug 12, 2012
+ */
+
+window.Event = window.Event || {};
+
+Event.addEventListenerToArray = function(arr, type, listener) {
+	for (var i = 0; i < arr.length; i++) {
+		var elem = arr[i];
+		Event.addEventListener(elem, type, listener);
+	};
+}
+
+Event.addEventListener = function(elem, type, listener) {
+	if (Object.prototype.toString.call( elem ) === '[object Array]') {
+		Event.addEventListenerToArray(elem, type, listener);
+		return;
+	}
+	if (!elem) {
+		return;
+	}
+	if (elem.addEventListener) {
+		elem.addEventListener(type, listener, false);
+	}
+	else if (elem.attachEvent) {
+		elem.attachEvent('on'+type, listener);
+	}
+	return elem;
+};
+
+Event.removeEventListener = function(elem, type, listener) {
+	if (!elem || !listener) {
+		return;
+	}
+	if (elem.removeEventListener) {
+		elem.removeEventListener(type, listener);
+	}
+	else if (elem.detachEvent) {
+		elem.detachEvent('on'+type, listener);
+	}
+	return elem;
+};
+
+Event.triggerEvent = function(elem, type) {
+	if (!elem) {
+		return;
+	}
+	var event;
+	if (document.createEvent) {
+		event = document.createEvent("HTMLEvents");
+		event.initEvent(type, true, true);
+	}
+	else {
+		event = document.createEventObject();
+		event.eventType = type;
+	}
+	if (document.createEvent) {
+		elem.dispatchEvent(event);
+	}
+	else {
+		elem.fireEvent("on" + event.eventType, event);
+	}
+	return elem;
+};
+
+Event.currentTarget = function(event) {
+	return !!event.currentTarget ? event.currentTarget : event.srcElement;
+};
